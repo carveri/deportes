e27a1002b0b3202c5d70449849630361e665d522
Carveri <carvajal.cortes.e19@gmail.com>
2025-11-15 15:13:02 -0300

    V4 con console.log del user

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@
 import express from "express";
 import futbolRoute from "../src/routes/futbol.route.js";
 
-
-
 const app = express()
 
 // middleware
 app.use(express.json())
 app.use('/api', futbolRoute)
+
+console.log('user:', process.env.USER_PG);
+
 
 
 app.listen(3000, ()=>{
